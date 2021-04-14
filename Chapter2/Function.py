ed2f4665d182b5cf2e90a45135b7268238254ffd
Humble Douglas <dollarkid17@gmail.com>
2021-04-14 10:42:08 +0100

    level 1

---
+++ Chapter2/Function.py
@@ -0,0 +1,7 @@
+
+
+def celsius_to_Fahrenheit():
+    a = int(input("enter a positive integer"))
+    return a*1.8+32.0
+
+print(celsius_to_Fahrenheit())
--- Chapter2/NameAndAge.py
+++ Chapter2/NameAndAge.py
@@ -5,3 +5,4 @@ for i in range (3):
     age=int(input('enter age'))
     user.append([name,age])
 print(user)
+print('d'+2)
